c3876d4fe20acdd920f8560de4dbaa49e0f66b75
Mubashir Hassan <official.mhm13@gmail.com>
2021-12-23 01:54:53 +0500

    Added Validation for NFT collection

--- model/Nft.js
+++ model/Nft.js
@@ -1,24 +1,60 @@
 const mongoose = require('mongoose');
 const dbCollections = require('../config/db_collections');
+const web3 = require('../config/web3');
 
 const nftSchema = new mongoose.Schema(
   {
-    name: String,
-    description: String,
-    token_name: String,
-    token_amount: Number,
+    name: {
+      type: String,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    token_name: {
+      type: String,
+      unique: true,
+      trim: true,
+      uppercase: true,
+    },
+    token_amount: {
+      type: Number,
+      min: 1,
+    },
     token_sold: {
       type: Number,
       default: 0,
+      min: 0,
     },
-    price: Number,
-    file_hash: String,
-    file_format: String,
-    owner: String,
-    metadata_hash: String,
+    price: {
+      type: Number,
+      min: 0.1,
+    },
+    file_hash: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    file_format: {
+      type: String,
+      trim: true,
+    },
+    metadata_hash: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    owner: {
+      type: String,
+      trim: true,
+      set: (val) => {
+        return web3.utils.toChecksumAddress(val);
+      },
+    },
     isDeleted: { type: Boolean, default: false },
   },
-  { timestamps: true, versionKey: false }
+  { timestamps: true }
 );
 
 const NFT = mongoose.model(dbCollections.NFT.model, nftSchema);
