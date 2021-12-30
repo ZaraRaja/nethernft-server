48c0ea686b4a3bb3fa925de011fa1112095f30e2
Mubashir Hassan <official.mhm13@gmail.com>
2021-12-30 21:21:25 +0500

    Ready to merge in main

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -4,7 +4,6 @@ const path = require('path');
 const storage = multer.diskStorage({
   destination: './uploads',
   filename: (req, file, cb) => {
-    console.log(file);
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
